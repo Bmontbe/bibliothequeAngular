8ceaeba822b5fdf96de41ff96c3fadfb87a67ade
bmontbe <bdemontbeillard@gmail.com>
2019-09-04 13:29:58 +0200

    add services and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bibliothequeAngular';
+  constructor() {
+    const config = {
+      apiKey: "AIzaSyBHcipj9PppdmhpaTTu7Gti5pQcr5kQ064",
+      authDomain: "bibliotheque-angular-c6475.firebaseapp.com",
+      databaseURL: "https://bibliotheque-angular-c6475.firebaseio.com",
+      projectId: "bibliotheque-angular-c6475",
+      storageBucket: "bibliotheque-angular-c6475.appspot.com",
+      messagingSenderId: "230650891576",
+      appId: "1:230650891576:web:e2553a9427712fdb"
+    };
+    firebase.initializeApp(config);
+  }
 }
